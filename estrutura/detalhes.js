4f2e2de93b173b9d3b1e2494c6f5198b2c8f6c20
Carolina Baião <carolinaabpessoa@gmail.com>
2023-06-01 14:49:26 -0300

    update telas

--- estrutura/detalhes.js
+++ estrutura/detalhes.js
@@ -1,11 +1,58 @@
-function IdUrl(url) {
-    const urlObj = new URL(url);
-    const path = urlObj.pathname;
-    const id = searchParams.get('id');
-    return id;
+// pegando id da url
+var url = window.location.href;
+var id = url.match(/id=(\d+)/)[1];
+
+console.log(url);
+console.log(id);
+
+// inserido elementos da api na tela
+function inserindoElementos(imagemUrl, titulo, descricao, preco, ano, avaliacao) {
+    const secao1 = document.getElementById('secao1');
+    const imagem = document.createElement('img');
+    imagem.src = imagemUrl;
+    imagem.classList.add('col-8');
+    secao1.appendChild(imagem);
+
+    const secao2 = document.getElementById('secao2');
+    const elementoTitulo = document.createElement('h1');
+    const elementoDescricao = document.createElement('p');
+    const elementoPreco = document.createElement('p');
+    const elementoAno = document.createElement('p');
+    const elementoAvaliacao = document.createElement('p');
+
+    elementoTitulo.textContent = titulo;
+    elementoDescricao.textContent = descricao;
+    elementoPreco.textContent = `Preço: ${preco}`;
+    elementoAno.textContent = `Ano de lançamento: ${ano}`;
+    elementoAvaliacao.textContent = `Nota de avaliação: ${avaliacao}`;
+
+    secao2.appendChild(elementoTitulo);
+    secao2.appendChild(elementoDescricao);
+    secao2.appendChild(elementoPreco);
+    secao2.appendChild(elementoAno);
+    secao2.appendChild(elementoAvaliacao);
 }
 
-function produtoApi(url) {
-    const id = IdUrl(url);
-    const apiUrl = `http://diwserver.vps.webdock.cloud:8765/products/${id}`; 
+// chamando api
+async function carregarConteudo() {
+    const apiUrl = `http://diwserver.vps.webdock.cloud:8765/products/${id}`;
+    console.log(apiUrl);
+
+    await fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const imagemUrl = data.image;
+            const titulo = data.title;
+            const descricao = data.description;
+            const preco = data.price;
+            const ano = data.year;
+            const avaliacao = data.rating.rate;
+
+            inserindoElementos(imagemUrl, titulo, descricao, preco, ano, avaliacao);
+        })
+        .catch(error => {
+            console.error(error);
+        })
 }
+carregarConteudo();
+
--- estrutura/script.js
+++ estrutura/script.js
@@ -1,11 +1,14 @@
+// declarando array (vazio) de produtos
 const products = [];
 
+// objeto que cria e adiciona o produto na dom e na tela
 const DOM = {
     productsContainer: document.querySelector('#products-list'),
 
     addProduct(product, index) {
         const element = document.createElement('div')
-        element.classList.add("col-3") 
+        element.classList.add("col-md-6")
+        element.classList.add("col-lg-4")
         element.innerHTML = DOM.innerHTMLProduct(product, index)
         element.dataset.index = index
 
@@ -13,9 +16,8 @@ const DOM = {
     },
 
     innerHTMLProduct(product, index) {
-        console.log(product);
         return `
-            <div class="card mb-2 m-2" style="width: 18rem; height: 60rem;">
+            <div class="product card mb-2 m-2">
                     <img src=${product.image} class="card-img-top"
                         alt="Imagem produto">
                     <div class="card-body">
@@ -31,6 +33,44 @@ const DOM = {
     }
 }
 
+// produtos do aside 
+const productsAside = [];
+
+const DOMaside = {
+    productsAsideContainer: document.querySelector('#aside-list'),
+
+    addProduct(productAside, index) {
+        const element = document.createElement('div')
+        element.classList.add("card-body")
+        element.classList.add("bg-light")
+        element.innerHTML = DOMaside.innerHTMLProduct(productAside, index)
+        element.dataset.index = index
+        DOMaside.productsAsideContainer.appendChild(element)
+    },
+
+    innerHTMLProduct(productAside, index) {
+        return `
+            <div class="flex-row">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <img src="${productAside.image}" class="img-fluid"
+                                            alt="Produto">
+                                    </div>
+                                    <div class="col-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">${productAside.title}</h5>
+                                            <p class="card-text fw-bold">${productAside.price}</p>
+                                            <a href="detalhes.html?id=${productAside.id}" data-product-id="${productAside.id}" target="_blank" class="btn btn-primary">Visualizar</a>
+                                        </div>
+                                    </div>
+                                </div>
+             </div>
+        `
+    }
+}
+
+
+// chamando api 
 async function init() {
     await fetch("http://diwserver.vps.webdock.cloud:8765/products/category/Personal Care - Skin Care")
         .then(async function (response) {
@@ -38,12 +78,35 @@ async function init() {
         })
         .then(function (response) {
             products.push(...response.products);
+            productsAside.push(...response.productsAside);
         })
         .catch(function (error) {
             console.log("Error: " + error);
         });
 
     products.forEach(product => DOM.addProduct(product));
+    productsAside.forEach(productAside => DOMaside.addProduct(productAside));
 }
 
-init() 
+init()
+
+// funcionalidade de pesquisa (input)
+const search = () => {
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const productslist = document.getElementById("products-list");
+    const product = document.querySelectorAll('.product');
+    const productname = productslist.getElementsByTagName("h5");
+
+    for (let i = 0; i < productname.length; i++) {
+        let combina = product[i].getElementsByTagName('h5')[0];
+        console.log(combina);
+        if (combina) {
+            let textvalue = combina.textContent || combina.innerHTML;
+            if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+                product[i].style.display = "";
+            } else {
+                product[i].style.display = "none";
+            }
+        }
+    }
+}
